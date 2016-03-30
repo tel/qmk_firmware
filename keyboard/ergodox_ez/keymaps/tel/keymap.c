ba05cd79d791604c96fd2a34a5a61a98157b96b0
Joseph Abrahamson <me@jspha.com>
2016-03-29 20:10:45 -0400

    update tel keymap

--- keyboard/ergodox_ez/keymaps/tel/keymap.c
+++ keyboard/ergodox_ez/keymaps/tel/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,       KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO,
         KC_TAB,        KC_Q, KC_W, KC_D, KC_F, KC_K, KC_FN1,
         CTL_T(KC_ESC), KC_A, KC_S, KC_E, KC_T, KC_G,
-        SFT_T(KC_ESC), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,
+        KC_LSFT,       KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,
 
         KC_NO, KC_NO, KC_NO, KC_LALT, KC_LGUI,
 
@@ -84,25 +84,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_TRNS,
         KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_TRNS,
         KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G,
-        KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,
+        KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_TRNS,
 
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-                       KC_NO, KC_NO,
-                              KC_NO,
-              KC_TRNS, KC_NO, KC_NO,
+                       KC_TRNS, KC_TRNS,
+                                KC_TRNS,
+              KC_TRNS, KC_TRNS, KC_TRNS,
 
         // right hand
-        KC_NO,   KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_TRNS,
-        KC_NO,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
+        KC_TRNS, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_TRNS,
+        KC_TRNS, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSLS,
                  KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-        KC_NO,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
+        KC_TRNS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS,
 
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-             KC_NO, KC_NO,
-             KC_NO,
-             KC_NO, KC_TRNS, KC_TRNS
+             KC_TRNS, KC_TRNS,
+             KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_TRNS
     ),
 };
 
